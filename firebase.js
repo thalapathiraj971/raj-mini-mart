236e37ae4ba4df6fd8a2c79020b5b979322fdb81
thalapathiraj971 <thalapathiraj971@gmail.com>
2026-07-08 02:25:18 +0530

    Add Firebase initialization in firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Import Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQehMWwcThf8NLMGeJIG-omcywEEiJpHs",
+  authDomain: "raj-mini-mart.firebaseapp.com",
+  projectId: "raj-mini-mart",
+  storageBucket: "raj-mini-mart.firebasestorage.app",
+  messagingSenderId: "490305070206",
+  appId: "1:490305070206:web:ff8214149720a7b8a1e42f"
+};
+
+const app = initializeApp(firebaseConfig);
+
+console.log("Firebase Connected Successfully!");
