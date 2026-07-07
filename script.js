5b6bfd7bd63abfef4fd6833036745d04f793f40c
thalapathiraj971 <thalapathiraj971@gmail.com>
2026-07-07 22:48:19 +0530

    Implement search functionality for products

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("RAJ MINI ONLINE MART Loaded Successfully");
+const search = document.getElementById("search");
+
+search.addEventListener("keyup", function () {
+    const value = search.value.toLowerCase();
+    const products = document.querySelectorAll(".product");
+
+    products.forEach(product => {
+        const text = product.innerText.toLowerCase();
+
+        if (text.includes(value)) {
+            product.style.display = "block";
+        } else {
+            product.style.display = "none";
+        }
+    });
 });
